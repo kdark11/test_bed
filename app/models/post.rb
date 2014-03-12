5bdb1dfeda89202b49516527cc3bbab346b5e443
Kevin Dark <k.dark11@gmail.com>
2014-03-12 05:11:54 +0000

    added error feedback to post controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,4 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  
-  def create
-    @post = Post.new(params[:post])
-    @post.save
-    redirect_to @post
-  end
-end
+  validates :body, length: { minimum: 10 }
+ end
